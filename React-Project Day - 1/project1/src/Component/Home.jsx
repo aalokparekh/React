b6afc75157d5368318a28ce045335d7bf0e1085b
meet <panchalmeetr97@gmail.com>
2024-04-26 16:58:39 +0530

    react

---
+++ React-Project Day - 1/project1/src/Component/Home.jsx
@@ -0,0 +1,18 @@
+function Home()
+{
+   return(<div style={{display:"flex", margin:"5%"}}>
+      <div className="main" style={{width:"60%"}}>
+      <p style={{textAlign:"left"}}>Alex Hormozi is an entrepreneur, investor, author of 3 bestselling books $100M Offers, Leads, and Money Models. He’s founded and exited 3 companies, the largest for $46.2M in 2021. He and his wife Leila are the managing partners of Acquisition.com - a portfolio of companies that generate in aggregate $200M per year. He also makes mistakes and candidly shares his painful lessons with other entrepreneurs. Today he publicly documents his lessons on his path from $100M net worth to $1B.<br/><br/>
+
+      Their large success, however, came from packaging and licensing his boutique gym model to 5000+ gyms around the world. They then started a supplement and software company to support that base of gyms. They sold a 66% stake in the company in 2021 at $46.2M to American Pacific Group. They then started Acquisition.com as a holding company for their private investments. The private equity firm focuses on making minority investments into cash flow positive growing founder owned businesses. They then scale those businesses. As of 2023, their portfolio of 16 companies generates $200,000,000 per year and growing.
+      <br/>  
+      The couple now has shifted their mission to making real business education available to everyone. They fulfill on this by making content across social media and publishing books and free courses for entrepreneurs.
+      <br/>
+      Alex’s most recent book $100M Offers: How to make offers so good people feel stupid saying no, has sold 300,000 copies from word of mouth alone. In their spare time, Alex and his wife still enjoy training at hardcore gyms and fending off death one workout at a time. </p>
+      </div>
+      <div className="sub-main" style={{width:"40%", backgroundImage:"cover"}}>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyd0QzbsJ6jAZEVh3odDo6PhnJ3fqrbjrewGA3ETVm8A&s" alt="" style={{height:"350px",cursor:"pointer"}} />
+      </div>
+      </div>)
+}
+export default Home;
