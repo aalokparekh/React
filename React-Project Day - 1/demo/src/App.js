eb0c208993430edafe280f7200e4b3057fb34fef
aalokparekh <5037aalokparekh@gmail.com>
2024-05-03 18:17:54 +0530

    react

---
+++ React-Project Day - 1/demo/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './Component/Footer';
+import Home from './Component/Home';
+import Header from './Component/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <Home />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
