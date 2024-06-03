dc9122a3d49e1d0bf1d94c7728162e3290e23949
aalokparekh <5037aalokparekh@gmail.com>
2024-06-03 10:42:54 +0530

    project3

---
+++ Integrate Bootstrap template/project3/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Nv from './component/Nv';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nv />
+    </div>
+  );
+}
+
+export default App;
